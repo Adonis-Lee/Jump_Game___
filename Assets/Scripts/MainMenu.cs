2b51e2f445bbffee34a1ae5e5013903293088c56
Alp Doruk <alpotheo@gmail.com>
2026-03-07 15:53:00 +0300

    Options / Sound Bar
     Basit ses ayarı düzeyi eklendi.

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -3,9 +3,34 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [Header("Paneller")]
+    [Tooltip("Options butonuna basılınca açılacak ses ayarları paneli")]
+    public GameObject optionsPanel;
+
+    void Start()
+    {
+        // Oyun başladığında panel kapalı olsun
+        if (optionsPanel != null)
+            optionsPanel.SetActive(false);
+    }
+
     public void Play()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    /// <summary>Options butonuna tıklanınca çağrılır.</summary>
+    public void OpenOptions()
+    {
+        if (optionsPanel != null)
+            optionsPanel.SetActive(true);
+    }
+
+    /// <summary>Geri butonuna tıklanınca çağrılır.</summary>
+    public void CloseOptions()
+    {
+        if (optionsPanel != null)
+            optionsPanel.SetActive(false);
     }
 
     public void Quit()
@@ -14,3 +39,4 @@ public class MainMenu : MonoBehaviour
         Debug.Log("Player has been quit");
     }
 }
+
